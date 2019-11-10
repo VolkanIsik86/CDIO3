b6269038d69e6398af041c783b462e75bbfb9f4d
mikkelwm <mikkelwm@gmail.com>
2019-11-10 13:56:25 +0100

    GameLogic

---
+++ src/main/java/game_classes/GameLogic.java
@@ -0,0 +1,16 @@
+package game_classes;
+
+import java.util.Scanner;
+
+public class GameLogic {
+    Private Piece piece = new Piece();
+    private Square[] fields = new Square[][24];
+    private Player[] players;
+    private Scanner input = new Scanner(System.in);
+    private TxtReader txt;
+
+    //Konstruktør
+    public GameLogic(){
+
+    }
+}
