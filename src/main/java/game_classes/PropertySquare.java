71a761f6e40c1fb5ef7321b4045c08466d6d0940
VolkanIsik86 <s180103@student.dtu.dk>
2019-11-11 19:54:09 +0100

    Property square

---
+++ src/main/java/game_classes/PropertySquare.java
@@ -0,0 +1,21 @@
+package game_classes;
+
+public class PropertySquare extends Square {
+
+
+    public PropertySquare(String name, int index, String color, int price, Player owner) {
+        super(name, index, color, price, owner);
+    }
+
+    public void landedOn(Piece p) {
+
+    }
+
+    public void payRent() {
+
+    }
+
+    public void getRent() {
+
+    }
+}
