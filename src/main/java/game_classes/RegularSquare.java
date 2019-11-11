80ae0aed3d96571c5c694cd94a1445c6510bf1b0
VolkanIsik86 <s180103@student.dtu.dk>
2019-11-11 19:11:39 +0100

    Regular square

---
+++ src/main/java/game_classes/RegularSquare.java
@@ -0,0 +1,13 @@
+package game_classes;
+
+public class RegularSquare extends Square {
+
+    public RegularSquare(String name, int index, String color) {
+        super(name, index, color);
+    }
+
+    @Override
+    public void landedOn() {
+        // do nothing
+    }
+}
