82e364e5c943779f6f0025b4bd8b73a6eff36606
mikkelwm <mikkelwm@gmail.co>
2019-11-11 11:09:10 +0100

    Start på Square

--- src/main/java/game_classes/GameLogic.java
+++ src/main/java/game_classes/GameLogic.java
@@ -6,7 +6,7 @@ public class GameLogic {
 
     private int startKapital;
     Private Piece piece = new Piece();
-    private Square[] fields = new Square[][24];
+    private Square[] squares = new Square[][24];
     private Player[] players;
     private Scanner input = new Scanner(System.in);
     private TxtReader txt;
