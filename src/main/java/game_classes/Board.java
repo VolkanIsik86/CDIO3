eaba7270f060632d2b5d6b57d445f2c1f7d6c0a6
Mik1812 <s183913@student.dtu.dk>
2019-11-12 15:56:07 +0100

    Udkast til konstruktor og getsquare skrevet i board

--- src/main/java/game_classes/Board.java
+++ src/main/java/game_classes/Board.java
@@ -5,29 +5,36 @@ import service.TxtReader;
 public class Board {
 
     private int SIZE = 24;
-    private Square[] square;
+    private Square[] squares;
     
     public Board(String path, String file){
     
-        TxtReader squareDescriptions = new TxtReader(path,file);
-        String[] line;
+        TxtReader sDec = new TxtReader(path,file);
+        String[] oneLine;
     
-        line = squareDescriptions.getLine("1").split(" ");
-    
-        System.out.println(line[2]);
-        
-//        for (int i = 0; i < 24; i++) {
-//            squares[i] = new Square()
-//        }
-    
-    
-    
+        for (int i = 0; i < 24; i++) {
+            
+            oneLine = sDec.getLine("1").split("-");
+            
+            switch (oneLine[0]) {
+                case ("Regular"):
+                    oneLine[i] = new Regular(sDec.getLine("1"),sDec.getLine("2"));
+                    break;
+                case("Property"):
+                    oneLine[i] = new Property(sDec.getLine("1"),sDec.getLine("2"),sDec.getLine("3"),sDec.getLine("4"));
+                    break;
+                case("Jail"):
+                    oneLine[i] = new Jail(sDec.getLine("1"),sDec.getLine("2"));
+                    break;
+                case("Chance"):
+                    oneLine[i] = new Chance(sDec.getLine("1"),sDec.getLine("2"));
+                    break;
+            }
+        }
     }
     
-    public static void main(String[] args) {
-        Board board = new Board("src/main/java/service/","squareDescriptions");
-        
-        
+    public Square getSquare(int index){
+        return squares[index+1];
     }
     
     //Skal have en .nextLocation(Square location, eyes)
