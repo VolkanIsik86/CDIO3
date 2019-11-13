e840e6b7b7db3282c78c4df5e00a1c882cc97bf0
Lasse <lasse@strunge.org>
2019-11-13 15:00:51 +0100

    tilføjet makeBoard under gui logic

--- src/main/java/controllers/GUILogic.java
+++ src/main/java/controllers/GUILogic.java
@@ -1,4 +1,32 @@
 package controllers;
 
+import gui_fields.GUI_Field;
+import gui_fields.GUI_Player;
+import gui_fields.GUI_Street;
+import gui_main.GUI;
+
+import java.awt.*;
+import java.util.Arrays;
+
+
 public class GUILogic {
+
+    public void makeBoard() {
+        GUI_Field[] fields = new GUI_Field[24];
+
+        //Dette er så simpelt som det kan blive.
+        for (int i = 0; i < 24; i++) {
+            fields[i] = new GUI_Street();
+
+            //Der tilføjes et par værdier.
+
+            GUI_Street testStreet = new GUI_Street();
+            testStreet.setTitle("Anker Engelundsvej");
+            testStreet.setBorder(Color.cyan); //Useful to show owner
+            testStreet.setRent("600,-");
+            fields[i] = testStreet;
+        }
+
+
+    }
 }
