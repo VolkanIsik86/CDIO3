38227f243c9a81602239bc913ab02a05a20f83de
Volkan Isik <volkanisik@DESKTOP-AEGI5D6.lan>
2019-11-14 21:01:56 +0100

    commented square classes

--- src/main/java/domain/squares/ChanceSquare.java
+++ src/main/java/domain/squares/ChanceSquare.java
@@ -5,11 +5,11 @@ import domain.Player;
 
 import java.util.Random;
 
-// chance square som inde holde chancekorte
+// Chance holds chance cards that affect player randomly
 public class ChanceSquare extends Square {
     ChanceCard [] chanceCards = new ChanceCard[4];
 
-// kontruktøren definere korterne her
+// Chance cards are defined her
     public ChanceSquare(String name, int index, Board board) {
         super(name, index, board);
         chanceCards[0] = new ChanceCard("Fødselsdag",0,2);
@@ -18,10 +18,14 @@ public class ChanceSquare extends Square {
         chanceCards[3] = new ChanceCard("Ryk 5 frem",5,0);
     }
 
+    // Pulls a random card from chancecards array and affects the player with it.
     public void landedOn(Player p) {
         int rng = new Random().nextInt(4);
         p.setLocation(board.nextLocation(this,chanceCards[rng].getMove()));
         p.addPoints((chanceCards[rng].getPrice()));
+        if(chanceCards[rng].getMove()>0){
+            p.getLocation().landedOn(p);
+        }
     }
 
     public ChanceCard[] getChanceCards() {
