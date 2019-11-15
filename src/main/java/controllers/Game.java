fdbe5854f305ce81db7f3190dec873b69d948e51
mikkelwm <mikkelwm@gmail.com>
2019-11-15 13:37:07 +0100

    gamelogic2

--- src/main/java/controllers/Game.java
+++ src/main/java/controllers/Game.java
@@ -9,10 +9,50 @@ import java.util.Scanner;
 
 public class Game {
 
-   GUILogic guiLogic = new GUILogic();
-   Board board = new Board(guiLogic.getPATH(),guiLogic.getFILE());
-   TurnLogic turnLogic = new TurnLogic(board);
-   PlayerList playerList = new PlayerList(board.getSquare(0));
+   GUILogic guiLogic;
+   Board board;
+   TurnLogic turnLogic;
+   PlayerList playerList;
+   int antalSpiller;
+   String[] spillernavne;
 
-   
-}
+   public void initGame(){
+       GUILogic guiLogic = new GUILogic();
+       Board board = new Board(guiLogic.getPATH(),guiLogic.getFILE());
+       TurnLogic turnLogic = new TurnLogic(board);
+       PlayerList playerList = new PlayerList(board.getSquare(0));
+       int antalSpiller;
+       String[] spillernavne = guiLogic.addPlayers(4);
+       setPlayername();
+   }
+
+
+   public void setPlayername() {
+       for (int i = 0; i < spillernavne.length; i++) {
+
+           //TODO player alder skal kunne defineres fra gui.
+           playerList.addPlayer(spillernavne[i], 0, 20);
+
+       }
+
+   }
+//    public void createPlayers() {
+//        System.out.println("");
+//
+//        //Point bliver bestemt efter antal spillere
+//        if (players.equals(2)){
+//            startKapital = 20;
+//        }
+//        if (players.equals(3)){
+//            startKapital = 18;
+//        }
+//        if (players.equals(4)){
+//            startKapital = 16;
+//        }
+//
+//        //Array af spillere laves
+//        for (int index = 0; index < players.length; index++) {
+//            final String navn = readPlayerName(index);
+////            players[index] = new Player(navn, startKapital);
+        }
+
