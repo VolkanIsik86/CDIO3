57ed4ca640a958b8129e03792847a6f7ab59d634
mikkelwm <mikkelwm@gmail.com>
2019-11-19 14:18:04 +0100

    Begyndelse på checkForWinnner

--- src/main/java/controllers/Game.java
+++ src/main/java/controllers/Game.java
@@ -17,7 +17,7 @@ public class Game {
    private PlayerList playerList;
    private String path = "src/main/java/services/";
    private String language;
-
+   private String winner;
    
     //todo ret antal startpoints
     private final int START_POINTS = 10;
@@ -44,8 +44,9 @@ public class Game {
 
                 guiLogic.update(currentPlayer, oldLocation, roll);
 
-                //todo check for winner --> break;
+                //todo check for winner --> break; Skal også vises i GUILogic!
                 if (currentPlayer.getPoints()<=0){
+                    winner = currentPlayer.getName();
 
                 }
             }
