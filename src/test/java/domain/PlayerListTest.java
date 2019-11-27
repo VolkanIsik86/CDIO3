3ae09b039683b7a657ad0d260c4f72b584a1282c
Volkan Isik <s180103@student.dtu.dk>
2019-11-27 15:43:03 +0100

    test af playerlist og de vigtige metoder

--- src/test/java/domain/PlayerListTest.java
+++ src/test/java/domain/PlayerListTest.java
@@ -1,33 +1,102 @@
 package domain;
 
+import controllers.GUILogic;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class PlayerListTest {
-    
-//    Board board = new Board("src/main/java/services/","squares");
-//    PlayerList players = new PlayerList(board.getStart());
-    
+    StubSquare testSquare;
+    GUILogic guiLogic;
+    PlayerList testlist = new PlayerList(testSquare,guiLogic);
+
     @Test
     public void addPlayer() {
-        
-//        //todo hurtig test, skal muligvis udbygges
-//
-//        players.addPlayer("navn",25);
-//        assertEquals("navn", players.getPlayer(0).getName());
-//        assertEquals(25, players.getPlayer(0).getAge());
-//        assertEquals(2000, players.getPlayer(0).getBalance());
-//        assertEquals(0, players.getPlayer(0).getType());
-//
-//        players.addPlayer("Hans",56);
-//        assertEquals("Hans", players.getPlayer(1).getName());
-//        assertEquals(56, players.getPlayer(1).getAge());
-//        assertEquals(1000, players.getPlayer(1).getBalance());
-//        assertEquals(1, players.getPlayer(1).getType());
+
+        testlist.addPlayer("test",99);
+        String expectedName = "test";
+        int expectedAge = 99;
+        assertEquals(Player.class,testlist.getPlayer(0).getClass());
+        assertEquals(expectedName,testlist.getPlayer(0).getName());
+        assertEquals(expectedAge,testlist.getPlayer(0).getAge());
+    }
+
+
+    @Test
+    public void numberOfPlayers() {
+        testlist.addPlayer("test",99);
+        int actual = testlist.NumberOfPlayers();
+        int expected = 1;
+        assertEquals(expected,actual);
+    }
+
+
+    @Test
+    public void sortPlayersByAge() {
+        testlist.addPlayer("test",7);
+        testlist.addPlayer("test1",8);
+        testlist.addPlayer("test2",5);
+        testlist.addPlayer("test3",4);
+        testlist.addPlayer("test4",9);
+        testlist.sortPlayersByAge();
+        int[] ages = new int[5];
+        for (int i = 0; i < testlist.NumberOfPlayers() ; i++){
+            ages[i] = testlist.getPlayer(i).getAge();
+        }
+
+        for (int i = 0; i < (ages.length - 1); i++){
+            assertEquals(true, ages[i] < ages[i + 1]);
+        }
+        assertEquals(true,ages[ages.length-2]<ages[ages.length-1]);
+
+    }
+    @Test
+    public void setStartBalance() {
+        testlist.addPlayer("test",7);
+        testlist.addPlayer("test1",8);
+        testlist.setStartBalance();
+        int expected = 20;
+        assertEquals(expected,testlist.getPlayer(0).getBalance());
+        assertEquals(expected,testlist.getPlayer(1).getBalance());
+    }
+
+    @Test
+    public void sortPlayersByPoint() {
+        testlist.addPlayer("test",7);
+        testlist.addPlayer("test1",8);
+        testlist.addPlayer("test2",5);
+        testlist.addPlayer("test3",4);
+        testlist.addPlayer("test4",9);
+        for (int i = 0; i < testlist.NumberOfPlayers() ; i++) {
+            testlist.getPlayer(i).setBalance(i);
+        }
+        testlist.sortPlayersByPoint();
+
+        int[] points = new int[5];
+        for (int i = 0; i < testlist.NumberOfPlayers() ; i++){
+            points[i] = testlist.getPlayer(i).getBalance();
+        }
+
+        for (int i = 0; i < (points.length - 1); i++){
+            assertEquals(true, points[i] < points[i + 1]);
+        }
+        assertEquals(true,points[points.length-2] < points[points.length-1]);
+    }
+
+    @Test
+    public void getWinner() {
+        testlist.addPlayer("test",7);
+        testlist.addPlayer("test1",8);
+
+        testlist.getPlayer(0).setBalance(20);
+        testlist.getPlayer(1).setBalance(18);
+
+        Player expected = testlist.getPlayer(0);
+        Player actual = testlist.getWinner();
+
+        assertEquals(expected,actual);
 
     }
 
-    
-    
+
 }
