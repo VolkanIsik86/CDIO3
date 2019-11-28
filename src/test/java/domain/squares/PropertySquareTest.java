502eb16a1530e05d5fb136dda91603ca239db532
VolkanIsik86 <s180103@student.dtu.dk>
2019-11-28 13:37:28 +0100

    ekstra test og fixed det med bordtesten

---
+++ src/test/java/domain/squares/PropertySquareTest.java
@@ -0,0 +1,50 @@
+package domain.squares;
+
+import controllers.GUILogic;
+import gui_main.GUI;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PropertySquareTest {
+
+    PropertySquare[] testSquare = new PropertySquare[2];
+    GUILogic guiLogic;
+
+
+
+    @Test
+    public void getColor() {
+        testSquare[0] = new PropertySquare("Test",0,guiLogic,)
+    }
+
+    @Test
+    public void getPrice() {
+    }
+
+    @Test
+    public void getOwner() {
+    }
+
+    @Test
+    public void setColor() {
+    }
+
+    @Test
+    public void setPrice() {
+    }
+
+    @Test
+    public void setOwner() {
+    }
+    @Test
+    public void payRent() {
+    }
+    @Test
+    public void earnRent() {
+    }
+    @Test
+    public void purchase(){
+
+    }
+}
