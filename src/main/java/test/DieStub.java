0f157f1b9e628762992e8fd120686b261de4b130
Mik1812 <s183913@student.dtu.dk>
2019-11-29 10:46:05 +0100

    Talrækker til de forskellige testcases i DieStub kommenteret

--- src/main/java/test/DieStub.java
+++ src/main/java/test/DieStub.java
@@ -1,15 +1,22 @@
 package test;
 
-import java.util.Random;
-
 public class DieStub {
     
-    private final int N_SIDES = 6;
     private int dieValue = 0;
-
-    //backendvsgui: {1, 1, 2, 4, 1, 4, 4, 3, 9, 11, 18, 1, 1, 1}
-    //1. PropertySquare.landedOn(): {1, 1, 23, 16, 1, 7, 13, 13, 8}
-    //2. PropertySquare.landedOn(): {1, 1, 23, 16, 1, 7, 13, 13, 2, 1}
+    
+    /*
+   // Talrækker til de forskellige testcases:
+   
+   T01 backendvsgui: {1, 1, 2, 4, 1, 4, 4, 3, 9, 11, 18, 1, 1, 1}
+   T02 1. PropertySquare.landedOn(): {1, 1, 23, 16, 1, 7, 13, 13, 8}
+   T02 2. PropertySquare.landedOn(): {1, 1, 23, 16, 1, 7, 13, 13, 2, 1}
+   T03 Flest point vinder:
+       2. spillere: {19}
+       3. spillere: {1, 19}
+       4. spillere: {1, 2, 19}
+       
+    */
+    
     private int[] rolls = {1, 1, 23, 16, 1, 7, 13, 13, 2, 1};
 
     private int counter = 0;
