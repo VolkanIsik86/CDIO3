2b4a583854e099f3c1e18c1accd715e0a5217f87
Mark-Laptop <bluedogdk@gmail.com>
2019-11-29 10:34:28 +0100

    PieceTest køres korrekt i alle tests nu

--- src/test/java/domain/PieceTest.java
+++ src/test/java/domain/PieceTest.java
@@ -16,12 +16,13 @@ public class PieceTest {
     private Piece testPiece1 = new Piece(square);
     private Piece testPiece2 = new Piece(square);
     private Piece testPiece3 = new Piece(square);
+    private int nrOfTypes = testPiece1.getType();
 
     @Test
     public void getType() {
-        assertEquals(0,testPiece1.getType());
-        assertEquals(1,testPiece2.getType());
-        assertEquals(2,testPiece3.getType());
+        assertEquals(nrOfTypes+0,testPiece1.getType());
+        assertEquals(nrOfTypes+1,testPiece2.getType());
+        assertEquals(nrOfTypes+2,testPiece3.getType());
 
 
     }
